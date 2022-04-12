87c269e0f8cc30697c522328598be6184043644c
Tristin Baker <tristinbaker52@gmail.com>
2022-04-12 17:47:48 -0400

    Add Alphabetical Bible

---
+++ alphabetical bible/alphabetical_bible.rb
@@ -0,0 +1,33 @@
+require 'pry'
+
+kjv = File.read("kjv.txt").gsub(/[^a-zA-Z]+/, ' ').split
+
+word_hash = Hash.new(0)
+
+kjv.each do |word|
+	word_hash[word] += 1
+end
+
+word_hash_alpha_sorted = word_hash.sort.to_h
+
+sorted = []
+
+word_hash_alpha_sorted.each do |word, count|
+	count.times do |i|
+		sorted << "#{word} "
+	end
+	sorted << "#{$/}"
+end
+
+word_hash_num_sorted = word_hash.sort_by {|k,v| v}.reverse.to_h
+
+stats = ""
+
+word_hash_num_sorted.each do |word, count|
+	stats += "#{word}: #{count}#{$/}"
+end
+
+#binding.pry
+
+File.open('sorted_kjv.txt', 'w') { |f| f.write sorted.join }
+File.open('stats.txt', 'w') { |f| f.write stats}
